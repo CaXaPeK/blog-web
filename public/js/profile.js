04553c4df8943fdb0ea03faa2a8f1f582cac10c1
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-12-07 17:29:56 +0700

    [Profile] Получение данных профиля
    Сделано получение и занесение в соответствующие поля следующих данных
    профиля: ФИО, почта, дата рождения, номер телефона, пол.

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,11 @@
 function getData() {
-    var apiUrl = 'https://blog.kreosoft.space/api/account/profile';
+    let apiUrl = 'https://blog.kreosoft.space/api/account/profile';
 
-    var inputName = document.getElementById('inputName');
-    var inputDate = document.getElementById('inputDate');
-    var inputGender = document.getElementById('inputGender');
-    var inputEmail = document.getElementById('inputEmail');
-    var inputNumber = document.getElementById('inputNumber');
+    let inputName = document.getElementById('inputName');
+    let inputDate = document.getElementById('inputDate');
+    let inputGender = document.getElementById('inputGender');
+    let inputEmail = document.getElementById('inputEmail');
+    let inputNumber = document.getElementById('inputNumber');
 
     $.ajax({
         url: apiUrl,
@@ -32,3 +32,38 @@ function sendAuthorizeCheck() {
     authorizeNavbar(true);
     getData();
 }
+
+document.getElementById('saveBtn').addEventListener('click',   function(event) {
+    let apiUrl = 'https://blog.kreosoft.space/api/account/profile';
+
+    let inputName = document.getElementById('inputName');
+    let inputDate = document.getElementById('inputDate');
+    let inputGender = document.getElementById('inputGender');
+    let inputEmail = document.getElementById('inputEmail');
+    let inputNumber = document.getElementById('inputNumber');
+
+    let postData = {
+        email: inputEmail.value,
+        fullName: inputName.value,
+        birthDate: inputDate.value,
+        gender: inputGender.value === 'male' ? 'Male' : 'Female',
+        phoneNumber: inputNumber.value
+    }
+
+    $.ajax({
+        url: apiUrl,
+        type: 'PUT',
+        dataType: 'json',
+        data: JSON.stringify(postData),
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Content-Type': 'application/json'
+        },
+        success: function(data) {
+            console.log("success");
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+});
